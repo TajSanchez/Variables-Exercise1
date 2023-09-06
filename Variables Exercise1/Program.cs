26abe81005ac1b723c7ae9798782a90cf43f1e9e
Taj <tajmarie92@gmail.com>
2023-09-05 22:04:06 -0500

    Add project files.

---
+++ Variables Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿string name;
+int age;
+char middleInitial;
+bool isOver18;
+double currentTemp;
+decimal currentPrice;
+
+//Declaring Variables (Lines 1-6)
+
+
+// Initializing Variables (Lines 9 -14)
+name = "Taj";
+age = 31;
+middleInitial = 'M';
+currentTemp = 73.6;
+currentPrice = 29.99m;
+isOver18 = true;
+
+// Writing The Variables To The Console (Lines 21-26)
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
